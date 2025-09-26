3bdc2fd52950f9528e5e1778569e1930e3be8bda
David Kartnaller <david.kartnaller@gmail.com>
2025-09-26 23:35:07 +0000

    add setup test

--- tests/e2e/e2e.spec.ts
+++ tests/e2e/e2e.spec.ts
@@ -5,6 +5,7 @@ describe("E2E Test", () => {
   
   process.env.DATABASE_PATH = "file::memory:?cache=shared"
   let app: AppService
+  let authToken: string
 
   beforeAll(async () => {
     app = new AppService()
@@ -20,5 +21,34 @@ describe("E2E Test", () => {
     expect(res.status).toBe(200)
     const data = await res.json()
     expect(data.setup).toBe(true)
+  })
+
+  it("should register for an account via setup", async () => {
+    const res = await fetch("http://localhost:8000/api/setup", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        username: "admin",
+        password: "foo123456"
+      })
+    })
+    expect(res.status).toBe(200)
+    const { user, token } = await res.json()
+    expect(user.id).toBe(1)
+    expect(user.username).toBe("admin")
+    expect(token).toBeTypeOf("string")
+    authToken = token
+  })
+
+  it("should validate that the setup route is not available anymore", async () => {
+    const res = await fetch("http://localhost:8000/api/setup", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        username: "admin",
+        password: "foo123456"
+      })
+    })
+    expect(res.status).toBe(404)
   })
 })
