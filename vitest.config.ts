07c9f11585717e824ba3ecf5893e0548ce6a6a7f
David Kartnaller <david.kartnaller@gmail.com>
2025-09-26 22:42:08 +0000

    add lcov to reporter for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     coverage: {
+      enabled: true,
       provider: "v8",
       include: ["src/*"],
-      reporter: ["text-summary"],
+      reporter: ["text-summary", "lcov"],
       skipFull: true
     },
   },
